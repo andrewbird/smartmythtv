8b813239c19fe5bdc5ee5e06d847e84ec1a4ccae
Steffen <s@s>
2012-07-01 14:21:42 +0200

    added semicolons

--- SmartMythTV/app/scenes/Main.js
+++ SmartMythTV/app/scenes/Main.js
@@ -6,27 +6,27 @@ function SceneMain() {
 
 SceneMain.prototype.initialize = function () {
 
-}
+};
 
 SceneMain.prototype.handleShow = function (data) {
 	alert("SceneMain.handleShow()");
 	// this function will be called when the scene manager show this scene 
-}
+};
 
 SceneMain.prototype.handleHide = function () {
 	alert("SceneMain.handleHide()");
 	// this function will be called when the scene manager hide this scene  
-}
+};
 
 SceneMain.prototype.handleFocus = function () {
 	alert("SceneMain.handleFocus()");
 	// this function will be called when the scene manager focus this scene
-}
+};
 
 SceneMain.prototype.handleBlur = function () {
 	alert("SceneMain.handleBlur()");
 	// this function will be called when the scene manager move focus to another scene from this scene
-}
+};
 
 SceneMain.prototype.handleKeyDown = function (keyCode) {
 	alert("SceneMain.handleKeyDown(" + keyCode + ")");
@@ -38,4 +38,4 @@ SceneMain.prototype.handleKeyDown = function (keyCode) {
 			alert("setting");
 			break;
 	}
-}
+};
