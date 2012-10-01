8cd7d825ac7143b5272f38e27a14b6907713e5b0
Steffen <Steffen@Steffen-Laptop>
2012-10-01 21:56:46 +0200

    optimized code

--- SmartMythTV/app/scenes/Recordings.js
+++ SmartMythTV/app/scenes/Recordings.js
@@ -91,16 +91,18 @@ SceneRecordings.prototype.handleKeyDown = function (keyCode) {
 		case sf.key.UP:
 			$('#svecScrollbar_UKRU').sfScroll('prev');
 			var idx = $('#svecListbox_BOUK').sfList('getIndex');
-			if(idx == 0) break;
-            $('#svecListbox_BOUK').sfList('prev');
-			SceneRecordings.prototype.showDescription();
+			if(idx != 0) {
+				$('#svecListbox_BOUK').sfList('prev');
+				SceneRecordings.prototype.showDescription();
+			}
 			break;
 		case sf.key.DOWN:
 			$('#svecScrollbar_UKRU').sfScroll('next');
 			var idx = $('#svecListbox_BOUK').sfList('getIndex');
-			if(idx == Data.max) break;
-            $('#svecListbox_BOUK').sfList('next');
-			SceneRecordings.prototype.showDescription();
+			if(idx != Data.max) {
+				$('#svecListbox_BOUK').sfList('next');
+				SceneRecordings.prototype.showDescription();
+			}
 			break;
 		case sf.key.ENTER:
 			Data.currentTitle = Data.Titles[$('#svecListbox_BOUK').sfList('getIndex')];
